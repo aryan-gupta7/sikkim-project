86b5386cdfb4fe89289bd9cecdf233476b3af1c6
aryan-gupta7 <71920399+aryan-gupta7@users.noreply.github.com>
2020-12-15 16:04:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import json
 app = Flask(__name__)
 
 file = open("animals.json","r")
-animal_info = json.load(file)
+animals = json.load(file)
+anis = animals['animals']
 
 @app.route("/")
 def main():
@@ -16,15 +17,22 @@ def intro():
 
 @app.route("/endangered-birds-and-animals")
 def edab():
-    return render_template("page2.html",animals=animal_info['animals'])
+    return render_template("page2.html",animals=animals['animals'])
 
 @app.route("/thanks_for_watching")
 def thanks():
     return render_template("last_page.html")
+
+@app.route("/animal_info/<animal_name>")
+def animal_info(animal_name):
+    for animal in anis:
+        animal = anis[animal]
+        if animal['sname'] == animal_name:
+            return render_template("show_animal.html",animal=animal)
 
 @app.errorhandler(404)
 def not_found(e):
     return redirect(url_for('main'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
