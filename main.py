7f4e8843b02b19c7b7f9a5b5a23aa4a3a27160f8
aryan-gupta7 <71920399+aryan-gupta7@users.noreply.github.com>
2020-12-15 13:16:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for
+import json
+
+app = Flask(__name__)
+
+file = open("animals.json","r")
+animal_info = json.load(file)
+
+@app.route("/")
+def main():
+    return render_template("title_page.html")
+
+@app.route("/intro")
+def intro():
+    return render_template("page1.html")
+
+@app.route("/endangered-birds-and-animals")
+def edab():
+    return render_template("page2.html",animals=animal_info['animals'])
+
+@app.route("/thanks_for_watching")
+def thanks():
+    return render_template("last_page.html")
+
+@app.errorhandler(404)
+def not_found(e):
+    return redirect(url_for('main'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
